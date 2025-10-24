d9e8febdeef5837ef65a0605c2c6fb3d51742260
Vishaka-M <marrivishaka23@gmail.com>
2025-10-24 15:37:54 -0500

    Updated backend routes and Mongo connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import interviewRoutes from "./routes/interviewRoutes.js"; // ✅ important
+import interviewRoutes from "./routes/interviewRoutes.js"; // important
 
 dotenv.config();
 const app = express();
@@ -11,21 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ connect route
+//  connect route
 app.use("/api/interviews", interviewRoutes);
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI || "";
 mongoose
   .connect(MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
 
 // Base route
 app.get("/", (req, res) => {
-  res.send("🚀 AI Interview Assistant Backend is Running Successfully...");
+  res.send("AI Interview Assistant Backend is Running Successfully...");
 });
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
